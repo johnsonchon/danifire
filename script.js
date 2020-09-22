824ebacd094c13e777e12f2835d353db6cb081b0
johnsonchon <39934039+johnsonchon@users.noreply.github.com>
2020-09-21 19:15:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const year = document.querySelector('#year');
+year.innerHTML = " " + new Date().getFullYear() + " ";
+
+const footBio = document.querySelector('#foot-bio');
+footBio.innerHTML = 'The essence of Dani Fire lies in our commitment to creating a powerful narrative for recreational and medical cannabis. At Dani Fire, we thrive on the notion that all cannabis products should be cultivated with quality in mind.'
